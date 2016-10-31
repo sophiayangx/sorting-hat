ce2a631a0322dd6f07b57af9edb0b57a6db22c45
sophiayangx <sopyang@nuevaschool.org>
2016-10-31 09:24:52 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ sortButton.click(function() {
 onNewListItem("enrolledStudents", function(student) {
   console.log("student", student.name, student.house)
 
-  var newStudentElement = $("<div class='enrolled-students'>" + student.name + " " + student.house + "</div>")
+  var newStudentElement = $("<div class='enrolled-students'>" +  student.name + " " + student.house + "</div>")
 
 
   enrolledStudentList.append(newStudentElement)
